d113d4d8cb11485aa2c36e26a5395b21bcb3d9e2
Zach Churchill <zachurchill@protonmail.com>
2024-08-09 22:24:56 -0400

    finish adding simple static pages and create Rubocop for lsp

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "application#hello"
+  root "static_pages#home"
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
