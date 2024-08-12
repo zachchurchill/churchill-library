5f8b2dda8d1d30b01dfb917e1fd96a74bee3d0fa
Zach Churchill <zachurchill@protonmail.com>
2024-08-11 22:13:20 -0400

    remove static pages example from book

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root "application#hello"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
