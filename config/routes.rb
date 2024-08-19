ccfba5f4cc107f61353d5ae7bce7571a895b116c
Zach Churchill <zachurchill@protonmail.com>
2024-08-19 09:00:07 -0400

    fix path to destroy session to logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "/about", to: "library#about"
   get "/admin", to: "sessions#new"
   post "/admin", to: "sessions#create"
-  delete "/admin", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
