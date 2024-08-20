03298682e4de5aecd02499854a23bf53139d40af
Zach Churchill <zachurchill@protonmail.com>
2024-08-19 21:58:02 -0400

    write first integration test to flash for invalid user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(name: params[:session][:username].downcase)
+    if user&.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page
+    else
+      flash.now[:danger] = "Invalid email/password combination"
+      render "new"
+    end
+  end
+
+  def delete
+  end
 end
