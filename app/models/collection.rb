de6cb07d3aba250c8dbe00fab066d52162b051bd
Zach Churchill <zachurchill@protonmail.com>
2024-08-27 08:02:19 -0400

    add basic validations on collections fields

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,6 @@
 class Collection < ApplicationRecord
+  validates :owner, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 200 }
+  validates :author, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :genre, allow_blank: true, length: { minimum: 3, maximum: 50 }
 end
--- test/models/collection_test.rb
+++ test/models/collection_test.rb
@@ -1,7 +1,63 @@
 require "test_helper"
 
 class CollectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @book = Collection.new(owner: "librarian", title: "book", author: "itself", genre: "metafiction")
+  end
+
+  test "should be valid" do
+    assert @book.valid?
+  end
+
+  test "owner should be present" do
+    @book.owner = nil
+    assert_not @book.valid?
+  end
+
+  test "owner should be >= 3 && <= 50 characters" do
+    @book.owner = "aa"
+    assert_not @book.valid?
+
+    @book.owner = "a" * 51
+    assert_not @book.valid?
+  end
+
+  test "title should be present" do
+    @book.title = nil
+    assert_not @book.valid?
+  end
+
+  test "title should be >=3 && <= 200 characters" do
+    @book.title = "a" * 2
+    assert_not @book.valid?
+
+    @book.title = "a" * 201
+    assert_not @book.valid?
+  end
+
+  test "author should be present" do
+    @book.author = nil
+    assert_not @book.valid?
+  end
+
+  test "author should be >= 3 && <= 100 characters" do
+    @book.author = "a" * 2
+    assert_not @book.valid?
+
+    @book.author = "a" * 101
+    assert_not @book.valid?
+  end
+
+  test "genre does not have to be present" do
+    @book.genre = nil
+    assert @book.valid?
+  end
+
+  test "genre should be >= 3 && <= 50 characters" do
+    @book.genre = "a" * 2
+    assert_not @book.valid?
+
+    @book.genre = "a" * 51
+    assert_not @book.valid?
+  end
 end
