8557f64afd050af9422904b70a3d9b6d454d02c3
Zach Churchill <zachurchill@protonmail.com>
2024-09-02 23:03:51 -0400

    finish first pass styling for collections page

--- test/controllers/collections_controller_test.rb
+++ test/controllers/collections_controller_test.rb
@@ -4,7 +4,7 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
   test "pagination is included and defaults to 10 records" do
     get collections_path
     assert_template "collections/show"
-    assert_select "div.pagination", count: 1
+    assert_select "div[aria-label='Pagination']", count: 1
     assert_select "tr[aria-label='book row']", count: 10
   end
 end
