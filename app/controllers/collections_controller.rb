7a655773b3dbecb9c14c69710c370f3f0cf4795b
Zach Churchill <zachurchill@protonmail.com>
2024-09-12 08:23:05 -0400

    wire up basic search functionality

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,7 +2,8 @@ class CollectionsController < ApplicationController
   def show
     @books = Collection.paginate(page: params[:page], per_page: 10)
 
-    # Filter based on selectors, allowing for multiple filters
+    # Filter based on search & selectors, allowing for multiple filters
+    @books = @books.where("title LIKE ?", "%#{params[:title]}%") if params[:title].present?
     @books = @books.where(owner: params[:owner]) if params[:owner].present?
     @books = @books.where(author: params[:author]) if params[:author].present?
     @books = @books.where.associated(:genres).where(genres: { name: params[:genre] }) if params[:genre].present?
