da49577b20f85ce27798437554702b66e5ee508e
Zach Churchill <zachurchill@protonmail.com>
2024-09-20 07:34:30 -0400

    rename helper function in filter tests for better reuse

--- test/integration/table_filtering_test.rb
+++ test/integration/table_filtering_test.rb
@@ -9,52 +9,46 @@ class TableFilteringTest < ActionDispatch::IntegrationTest
     get collections_path
     assert_select "select[id='owner']"
     assert_select "select[id='owner']>option", count: 5
-    assert_select_option "owner", "", @page_size
-    assert_select_option "owner", "zach", 2
-    assert_select_option "owner", "courtney", 2
-    assert_select_option "owner", "penelope", 1
-    assert_select_option "owner", "system", @page_size
-    assert_select_option "owner", "", @page_size
+    assert_filter "owner", "", @page_size
+    assert_filter "owner", "zach", 2
+    assert_filter "owner", "courtney", 2
+    assert_filter "owner", "penelope", 1
+    assert_filter "owner", "system", @page_size
+    assert_filter "owner", "", @page_size
   end
 
   test "selection of author correctly filters table" do
     get collections_path
     assert_select "select[id='author']"
     assert_select "select[id='author']>option", count: 5
-    assert_select_option "author", "", @page_size
-    assert_select_option "author", "erb", @page_size
-    assert_select_option "author", "rebecca yarros", 2
-    assert_select_option "author", "stephen king", 2
-    assert_select_option "author", "dr. seuss", 1
+    assert_filter "author", "", @page_size
+    assert_filter "author", "erb", @page_size
+    assert_filter "author", "rebecca yarros", 2
+    assert_filter "author", "stephen king", 2
+    assert_filter "author", "dr. seuss", 1
   end
 
   test "selection of genre correctly filters table" do
     get collections_path
     assert_select "select[id='genre']"
     assert_select "select[id='genre']>option", count: 5
-    assert_select_option "genre", "", @page_size
-    assert_select_option "genre", "horror", @page_size
-    assert_select_option "genre", "fantasy", 2
-    assert_select_option "genre", "romance", 2
-    assert_select_option "genre", "childrens", 1
+    assert_filter "genre", "", @page_size
+    assert_filter "genre", "horror", @page_size
+    assert_filter "genre", "fantasy", 2
+    assert_filter "genre", "romance", 2
+    assert_filter "genre", "childrens", 1
   end
 
   test "search is case insensitive" do
     get collections_path
     assert_select "input[id='title']"
-    get collections_path, params: { "title": "fourth" }
-    assert_select "tr[aria-label='book row']"
-
-    get collections_path, params: { "title": "" }
-    assert_select "tr[aria-label='book row']", @page_size
-
-    get collections_path, params: { "title": "Fourth" }
-    assert_select "tr[aria-label='book row']"
+    assert_filter "title", "fourth", 1
+    assert_filter "title", "", @page_size
+    assert_filter "title", "Fourth", 1
   end
 
   test "search and filters work together to filter" do
-    get collections_path, params: { "title": "ng" }
-    assert_select "tr[aria-label='book row']", count: 2
+    assert_filter "title", "ng", 2
 
     get collections_path, params: { "title": "ng", "owner": "zach" }
     assert_select "tr[aria-label='book row']"
@@ -62,8 +56,8 @@ class TableFilteringTest < ActionDispatch::IntegrationTest
 
   private
 
-  def assert_select_option(select_id, option, count)
-    get collections_path, params: { "#{select_id}": option }
+  def assert_filter(param, value, count)
+    get collections_path, params: { "#{param}": value }
     assert_select "tr[aria-label='book row']", count
   end
 end
