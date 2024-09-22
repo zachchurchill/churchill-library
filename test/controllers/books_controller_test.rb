2472f37a88c45f3032bab4d1cfb433d556167e18
Zach Churchill <zachurchill@protonmail.com>
2024-09-22 14:18:00 -0400

    improve book controller test random string generator

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -21,11 +21,11 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
 
   test "correctly adds book upon page submission" do
     login
-    genre = generate_random_string(8)
+    genre = generate_random_string
     new_book = {
-      owner: generate_random_string(5),
-      title: generate_random_string(20),
-      author: generate_random_string(10),
+      owner: generate_random_string,
+      title: generate_random_string,
+      author: generate_random_string,
       genres: genre
     }
     post book_path, params: new_book
@@ -34,12 +34,12 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     assert_not_nil Genre.find_by(name: genre)
     assert_not_nil Book.find_by(title: new_book[:title])
 
-    main_genre = generate_random_string(9)
-    sub_genre = generate_random_string(7)
+    main_genre = generate_random_string
+    sub_genre = generate_random_string
     another_book = {
-      owner: generate_random_string(10),
-      title: generate_random_string(40),
-      author: generate_random_string(12),
+      owner: generate_random_string,
+      title: generate_random_string,
+      author: generate_random_string,
       genres: "#{main_genre}, #{sub_genre}"
     }
     post book_path, params: another_book
@@ -52,22 +52,22 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
 
   test "multiple books for owner uses same owner record" do
     login
-    owner = generate_random_string(5)
+    owner = generate_random_string
     new_book = {
-      title: generate_random_string(20),
+      title: generate_random_string,
       owner: owner,
-      author: generate_random_string(10),
-      genres: generate_random_string(8)
+      author: generate_random_string,
+      genres: generate_random_string
     }
     assert_difference "Owner.count" do
       post book_path, params: new_book
     end
 
     another_book = {
-      title: generate_random_string(24),
+      title: generate_random_string,
       owner: owner,
-      author: generate_random_string(12),
-      genres: generate_random_string(5)
+      author: generate_random_string,
+      genres: generate_random_string
     }
     assert_no_difference "Owner.count" do
       post book_path, params: another_book
@@ -76,22 +76,22 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
 
   test "multiple books for author uses same author record" do
     login
-    author = generate_random_string(5)
+    author = generate_random_string
     new_book = {
-      title: generate_random_string(20),
-      owner: generate_random_string(10),
+      title: generate_random_string,
+      owner: generate_random_string,
       author: author,
-      genres: generate_random_string(8)
+      genres: generate_random_string
     }
     assert_difference "Author.count" do
       post book_path, params: new_book
     end
 
     another_book = {
-      title: generate_random_string(24),
-      owner: generate_random_string(12),
+      title: generate_random_string,
+      owner: generate_random_string,
       author: author,
-      genres: generate_random_string(5)
+      genres: generate_random_string
     }
     assert_no_difference "Author.count" do
       post book_path, params: another_book
@@ -100,23 +100,23 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
 
   test "multiple books with overlapping genres uses same previous genre records" do
     login
-    main_genre = generate_random_string(9)
-    sub_genre = generate_random_string(7)
+    main_genre = generate_random_string
+    sub_genre = generate_random_string
     new_book = {
-      title: generate_random_string(20),
-      owner: generate_random_string(14),
-      author: generate_random_string(10),
+      title: generate_random_string,
+      owner: generate_random_string,
+      author: generate_random_string,
       genres: "#{main_genre}, #{sub_genre}"
     }
     assert_difference "Genre.count", 2 do
       post book_path, params: new_book
     end
 
-    other_sub_genre = generate_random_string(12)
+    other_sub_genre = generate_random_string
     another_book = {
-      title: generate_random_string(24),
-      owner: generate_random_string(14),
-      author: generate_random_string(12),
+      title: generate_random_string,
+      owner: generate_random_string,
+      author: generate_random_string,
       genres: "#{main_genre}, #{other_sub_genre}"
     }
     assert_difference "Genre.count" do
@@ -124,9 +124,9 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     end
 
     final_book = {
-      title: generate_random_string(34),
-      owner: generate_random_string(4),
-      author: generate_random_string(19),
+      title: generate_random_string,
+      owner: generate_random_string,
+      author: generate_random_string,
       genres: "#{sub_genre}, #{other_sub_genre}"
     }
     assert_no_difference "Genre.count" do
@@ -136,7 +136,8 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
 
   private
 
-  def generate_random_string(length)
+  def generate_random_string(length = nil)
+    length = Random.rand(3..50) if length.nil?
     alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
     (1..length).map { alphabet.sample }.join
   end
