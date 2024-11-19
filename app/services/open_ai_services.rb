1d5aa74eeef1f67ed2692a58f465f96d295dc15a
Zach Churchill <zachurchill@protonmail.com>
2024-11-18 22:15:40 -0500

    Add chat method to OpenAiServices to get started with chat feature

--- app/services/open_ai_services.rb
+++ app/services/open_ai_services.rb
@@ -18,4 +18,21 @@ class OpenAiServices
     )
     embedding.dig("data", 0, "embedding")
   end
+
+  def chat(conversation)
+    system_prompt = <<~PROMPT.strip
+      You are an assistant for a personal library collection app.
+      This application catalogues the books for various owners where the title, author and genres are saved.
+      Users may have queries around what kind of books the owners of these collections own and would be interested in.
+      Be helpful, concise and sincere.
+    PROMPT
+    resp = @client.chat(
+      parameters: {
+        model: "gpt-4o-mini",
+        messages: [{ role: "system", content: system_prompt }, *conversation.messages],
+        temperature: 0.6
+      }
+    )
+    conversation.add_assistant_message(resp.dig("choices", 0, "message", "content"))
+  end
 end
