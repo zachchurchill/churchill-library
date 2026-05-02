365aa38d117844a277008be59781f97182521cc1
zachchurchill <zachurchill@protonmail.com>
2026-05-01 22:39:41 -0400

    Make books the home page

--- test/controllers/library_controller_test.rb
+++ test/controllers/library_controller_test.rb
@@ -4,8 +4,16 @@ class LibraryControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", root_title
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "title", "Books | #{root_title}"
+    assert_select "h1", "Welcome to the Library"
+    assert_select "p", "The Churchill Library houses books from all our family members. Explore our books using the provided search and filters."
+    assert_select "input[id='title']"
+    assert_select "select[id='owner']"
+    assert_select "select[id='author']"
+    assert_select "select[id='genre']"
+    assert_select "tr[aria-label='book row']"
+    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", books_path, count: 0
     assert_select "a[href=?]", admin_path
   end
 end
